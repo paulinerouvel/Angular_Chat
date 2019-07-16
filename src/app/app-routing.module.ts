1819f97756f382277d473ced579e38cfa3a3f752
Grégory <gregory.brugnet@gmail.com>
2019-07-16 08:10:38 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Core/component/home/home.component';
 import { LoginComponent } from './Core/component/login/login.component';
+import { MenuAdminComponent } from './Administrateur/component/menu-admin/menu-admin.component';
+import { GestionPropositionComponent } from './Administrateur/component/gestion-proposition/gestion-proposition.component';
+import { BoutiqueComponent } from './Visiteur/component/boutique/boutique.component';
+import { MesCommandesComponent } from './Visiteur/component/mes-commandes/mes-commandes.component';
+import { GestionUtilisateurComponent } from './Administrateur/component/gestion-utilisateur/gestion-utilisateur.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"/home", pathMatch:"full"},
   {path:"home", component: HomeComponent},
-  {path:"login", component: LoginComponent}
+  {path:"login", component: LoginComponent},
+  {path:"admin", component: MenuAdminComponent},
+  {path:"admin/gestion-proposition", component: GestionPropositionComponent},
+  {path:"admin/gestion-utilisateur", component: GestionUtilisateurComponent},
+  {path:"boutique", component: BoutiqueComponent},
+  {path:"mesCommandes", component: MesCommandesComponent},
+  {path:"admin", component: MenuAdminComponent},
+  {path:"admin", component: MenuAdminComponent},
+  
+
 ];
 
 @NgModule({
